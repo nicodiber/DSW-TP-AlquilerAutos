6d482ecac7c42ef8ceee3244497fc0ae24da92f8
nicodiber <ndibernardo@gmail.com>
2024-11-03 20:03:49 -0300

    Update sucursal-editar.component.ts

--- frontend/src/app/components/sucursal-editar/sucursal-editar.component.ts
+++ frontend/src/app/components/sucursal-editar/sucursal-editar.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SucursalService } from '../../services/sucursal.service';
+import { sucursal } from '../../models/sucursal';
 
 @Component({
   selector: 'app-sucursal-editar',
   templateUrl: './sucursal-editar.component.html',
-  styleUrl: './sucursal-editar.component.css'
+  styleUrls: ['./sucursal-editar.component.css']
 })
-export class SucursalEditarComponent {
+export class SucursalEditarComponent implements OnInit {
+  idSucursal: string = '';
+  sucursalData: sucursal | null = null; // Inicializa con null
+  mensaje: string | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private sucursalService: SucursalService
+  ) { }
+
+  ngOnInit(): void {
+    this.idSucursal = this.route.snapshot.paramMap.get('id') || '';
+    this.obtenerSucursal();
+  }
+
+  obtenerSucursal(): void {
+    this.sucursalService.obtenerSucursal(this.idSucursal).subscribe(
+      (data) => {
+        this.sucursalData = data;
+      },
+      (error) => {
+        this.mensaje = 'Error al obtener la sucursal';
+        console.error(error);
+      }
+    );
+  }
+
+  editarSucursal(): void {
+    if (this.sucursalData) {
+      this.sucursalService.editarSucursal(this.idSucursal, this.sucursalData).subscribe(
+        (response) => {
+          this.mensaje = 'Sucursal actualizada con éxito';
+          this.router.navigate(['/sucursal-listar']); // Redirigir a la lista de sucursales
+        },
+        (error) => {
+          this.mensaje = 'Error al actualizar la sucursal';
+          console.error(error);
+        }
+      );
+    }
+  }
 }
