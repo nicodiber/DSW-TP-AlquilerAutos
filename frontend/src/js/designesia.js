c165f1339efaa413e03cec2fd54dea71049f4c7b
BrunoNeirotti <bneirotti@gmail.com>
2024-11-08 09:04:22 -0300

    Cambios Merge Ajustes-Modelo

--- frontend/src/js/designesia.js
+++ frontend/src/js/designesia.js
@@ -7,7 +7,7 @@
      var rtl_mode = 'off'; // on - for enable RTL, off - for deactive RTL
      var preloader = 'on'; // on - for enable preloader, off - for disable preloader
      var preloader_custom_image = 'off'; // insert image url to enable custom image, off - for disable custom image
-     var loading_text = "Loading..."; // text for preloader. If you don't use text just leave it blank
+     var loading_text = "Cargando..."; // text for preloader. If you don't use text just leave it blank
      var loading_text_position = "0px"; // set position for loading text. Default value is 0px
 
      /* predefined vars begin */
