340631908ca7915446dc24f75b3fd9e100cf50af
nicodiber <ndibernardo@gmail.com>
2024-12-14 13:37:50 -0300

    Update mantenimiento.js

--- backend/routes/mantenimiento.js
+++ backend/routes/mantenimiento.js
@@ -2,12 +2,6 @@ const express = require('express');
 const router = express.Router();
 const imantenimientoController = require('../controllers/mantenimientoController');
 
-// Middleware para ver si llega la solicitud
-router.use((req, res, next) => {
-  console.log('Request received:', req.method, req.originalUrl);  // Verifica si se recibe la solicitud
-  next();
-});
-
 router.post('/', imantenimientoController.crearMantenimiento);
 router.post('/mantenimientoAlquiler/:idAuto', imantenimientoController.crearMantenimientoAlquiler);
 router.get('/', imantenimientoController.obtenerMantenimientos);                                        // Obtiene todos los mantenimientos
